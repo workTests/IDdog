aec1eea4b09e8e55e9f0093c3d3257973afa6dd2
Mara Oliveira <mara.15oliveira@outlook.com>
2018-07-25 17:06:37 -0300

    install react-dom - Homepage start

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
+import * as axios from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Fragment
+} from 'react-router-dom'
+
+//CSS
 import './css/App.css';
+import './css/style.css';
+
+//PAGES
+import Homepage from './pages/Homepage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+       <React.Fragment>  
+          <Router>
+            <Switch>
+              <div>
+                The IDDog
+              </div>
+            </Switch>
+          </Router>
+       </React.Fragment>
     );
   }
 }
---
+++ my-app/src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Fragment
+} from 'react-router-dom'
+
+class Homepage extends Component {
+
+}
+
+export default Homepage;
